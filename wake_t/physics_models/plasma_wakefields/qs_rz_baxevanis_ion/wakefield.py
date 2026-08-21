7294c85f2d530bffe46a131e4839cb6a7e5e4eb4
Alexander Sinn <alexander.sinn@desy.de>
2026-08-21 11:04:36 +0200

    Update all field_diags doc strings

--- wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/wakefield.py
+++ wake_t/physics_models/plasma_wakefields/qs_rz_baxevanis_ion/wakefield.py
@@ -128,7 +128,21 @@ class Quasistatic2DWakefieldIon(RZWakefield):
         solver.
     field_diags : list, optional
         List of fields to save to openpmd diagnostics. By default ['rho', 'E',
-        'B', 'a_mod', 'a_phase'].
+        'B', 'a_mod', 'a_phase']. Can also be 'all'.
+        Each entry can also be a dict to give more precise control of the
+        outputted data. The dict can have the following keys:
+        {
+            "field" : list of field names to output
+            "r_min" , "r_max" , "xi_min" , "xi_max" :
+                Cut the diagnostic box in r and xi.
+            "r_stride" , "xi_stride" :
+                Add a stride in units of dr and dxi to r and xi.
+            "do_transpose" :
+                Wheater to transpose data for the output (default True).
+            "diag_name" :
+                Name to append to the field name if there are multiple
+                diagnostics with the same field.
+        }
     particle_diags : list, optional
         List of particle quantities to save to openpmd diagnostics. By default
         [].
